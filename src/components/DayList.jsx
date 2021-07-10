bb97ddca26c45edb09c7a86800671da8a6767330
Abanoub Shenouda <Abanoub Shenouda>
2021-07-10 17:54:16 -0400

    Remove unnecessary comments

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -3,16 +3,16 @@ import DayListItem from "./DayListItem";
 //import {days} from "../../stories/index";
 
 export default function DayList(props) {
-    const {days} = props;
+    const { days } = props;
 
     return (
         <ul>
             {days.map(day => {
-                return(<DayListItem
+                return (<DayListItem
                     name={day.name}
                     spots={day.spots}
                     selected={day.name === props.day}
-                    setDay={event=>{props.setDay(day.name)}} />)
+                    setDay={event => { props.setDay(day.name) }} />)
             }
             )}
         </ul>
