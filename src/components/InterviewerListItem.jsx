b61c7a306436ab6a0c675c5cb1495d247fa4be57
Abanoub Shenouda <Abanoub Shenouda>
2021-07-10 17:52:32 -0400

    Refactor InterviewerList and InterviewerList item for Form Component props

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -4,10 +4,14 @@ import classNames from "classnames";
 
 const InterviewerListItem = (props) => {
 
+    // id:number - the id of the interviewer
+    // name:string - the name of the interviewer
+    // avatar:url - a url to an image of the interviewer
+    // selected:boolean - to determine if an interview is selected or not
+    // setInterviewer:function - sets the interviewer upon selection
+
     const {name, avatar, selected, setInterviewer } = props;
 
-
-    //const onClickHandle()
     const classNameItem = classNames("interviewers__item", {
         "interviewers__item--selected": selected
 
@@ -15,15 +19,7 @@ const InterviewerListItem = (props) => {
     const classNameImage = classNames("interviewers__item-image", {
         "interviewers__item-image--selected": selected
     })
-    // id:number - the id of the interviewer
-    // name:string - the name of the interviewer
-    // avatar:url - a url to an image of the interviewer
-    // selected:boolean - to determine if an interview is selected or not
-    // setInterviewer:function - sets the interviewer upon selection
 
-    // const handleSetInterViewer = () => {
-    //     setInterviewer(id);
-    // }
 
     return (<li className={classNameItem} onClick={setInterviewer} >
         <img
