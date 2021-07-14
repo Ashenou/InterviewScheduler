cf6ce9bb348506ef55a23cea4479a01073bbf517
Abanoub Shenouda <Abanoub Shenouda>
2021-07-14 19:24:22 -0400

    Add stories to test useVisualMode custom hook

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -21,6 +21,41 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
 import Form from "components/Appointment/Form";
+
+
+// DayList stories
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
+];
+
+
+
+
+
+
 
 // Button stories
 storiesOf("Button", module)
@@ -52,24 +87,6 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
 
-// DayList stories
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 storiesOf("DayList", module)
   .addParameters({
@@ -117,14 +134,12 @@ storiesOf("InterviewerListItem", module)
     />
   ));
 
+
+
+
+
 // InterviewerList stories
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
-];
+
 
 storiesOf("InterviewerList", module)
   .addParameters({
@@ -144,12 +159,32 @@ storiesOf("InterviewerList", module)
     />
   ));
 
+
+
+
+
+  
 // Appointment Stories
 storiesOf("Appointment", module)
   .addParameters({
     backgrounds: [{ name: "white", value: "#fff", default: true }],
   })
   .add("Appointment", () => <Appointment />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  )).add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
@@ -176,7 +211,6 @@ storiesOf("Appointment", module)
       onClose={action("OnClose")}
     />
   ));
-
 // Form Stories (Edit, Create)
 storiesOf("Form", module)
   .add("Edit", () => (
@@ -184,14 +218,14 @@ storiesOf("Form", module)
       name={"Lydia Miller-Jones"}
       interviewers={interviewers}
       interviewer={interviewer.id}
-      onSave={action("onSave")}
+      onSubmit={action(onsubmit)}
       onCancel={action("onCancel")}
     />
   ))
   .add("Create", () => (
     <Form
       interviewers={interviewers}
-      onSave={action("onSave")}
+      onSubmit={action("onSave")}
       onCancel={action("onCancel")}
     />
   ));
