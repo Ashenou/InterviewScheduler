f2e644bf10d8324ce420868a0667b5424100e8fe
Abanoub Shenouda <Abanoub Shenouda>
2021-07-15 17:37:37 -0400

    Add addInterview conditional rendering and function calling logic

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,57 +1,65 @@
-// The Form component should track the following state:
-
-// name:String
-// interviewer:Number
-// The Form component should have the following actions:
-
-// setName:Function
-// setInterviewer:Function
-
-
-// As part of our Edit story, the Form component should take the following props:
-// name:String
-// interviewers:Array
-// interviewer:Number
-// onSave:Function
-// onCancel:Function
-
-// As part of our Create story, the Form component should take the following props:
-// interviewers:Array
-// onSave:Function
-// onCancel:Function
-
-
 import React, { useState } from 'react';
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
 const Form = (props) => {
+    // The Form component should track the following state:
+
+    // name:String
+    // interviewer:Number
+    // The Form component should have the following actions:
+
+    // setName:Function
+    // setInterviewer:Function
+
+
+    // As part of our Edit story, the Form component should take the following props:
+    // value:String //name
+    // interviewers:Array
+    // interviewer:Number
+    // onSave:Function
+    // onCancel:Function
+
+    // As part of our Create story, the Form component should take the following props:
+    // interviewers:Array
+    // onSave:Function
+    // onCancel:Function
+
 
     // or sets it as -1 if nothing is passed in
-    const [interviewer, setInterviewer] = useState(props.interviewer || -1);
     const [name, setName] = useState(props.name || "");
+    const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
-    console.log(props.interviewer);
+    // This is one of the benefits of using the controlled components pattern. We are not only able to change the state when the input field changes. With this pattern, we can also have the input field update based on a change of state.
+
+    const Reset = () => {
+        setName("");
+        setInterviewer(null);
+        props.onCancel();
+    }
+    /*No Complete behaviour */
+    // const Save = (event) => {
+    //     //event.preventDefault();
+    //     props.onSave(name,interviewer);
+    // }
+
     return (<main className="appointment__card appointment__card--create">
         <section className="appointment__card-left">
-            <form autoComplete="off">
+            <form autoComplete="off" onSubmit={event => event.preventDefault()}>
                 <input
                     className="appointment__create-input text--semi-bold"
                     value={name}
-                    onChange={(event)=>setName(event.target.value)}
+                    onChange={(event) => setName(event.target.value)}
                     type="text"
                     placeholder="Enter Student Name"
-                /*
-                  This must be a controlled component
-                */
                 />
             </form>
             <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
         </section>
         <section className="appointment__card-right">
             <section className="appointment__actions">
-                <Button danger>Cancel</Button>
-                <Button confirm>Save</Button>
+                <Button danger onClick={() => Reset()} >Cancel</Button>
+                <Button confirm onClick={()=> props.onSave(name,interviewer)}>Save</Button>
             </section>
         </section>
     </main>)
