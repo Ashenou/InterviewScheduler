afd3dd89ca3d27d5cfde03b75c1940037c5aa117
Abanoub Shenouda <Abanoub Shenouda>
2021-07-15 13:26:33 -0400

    Fix indentation

--- src/components/Appointment/Empty.jsx
+++ src/components/Appointment/Empty.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+
 const Empty = (props) => {
+
+  let mainClassName = "appointment__add";
+  let imgClassName = "appointment__add-button"
+
+  // if (props.id === '5') {
+  //   mainClassName += ":last-of-type";
+  //   imgClassName += ":last-of-type";
+  // }
+
   return (
-    <main className="appointment__add">
+    <main className={mainClassName}>
       <img
-        className="appointment__add-button"
+        className={imgClassName}
         src="images/add.png"
         alt="Add"
         onClick={props.onAdd}
