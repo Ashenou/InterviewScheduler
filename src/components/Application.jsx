5b8495e9f96f028910613e8ac08dd9c321e252d5
Abanoub Shenouda <Abanoub Shenouda>
2021-07-15 13:22:17 -0400

    Add bookInterview function

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,30 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import DayList from "components/DayList";
 import "components/Application.scss";
-//import Appointment from "components/Appointment";
+import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import { getInterview } from "../helpers/getInterview.js";
+import { getAppointmentsForDay } from "../helpers/getAppointmentsForDay";
+import { getInterviewersForDay } from "../helpers/getInterviewersForDay";
+
+
+
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Mildred Nazir",
+//         avatar: "https://i.imgur.com/T2WwVfS.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 5,
+//         name: "Sven Jones",
+//         avatar: "https://i.imgur.com/twYrpay.jpg"
+//       }
+//     }
+//   }
+// ];
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+
+
+  useEffect(() => {
+    Promise.all([axios.get("api/days"), axios.get("api/appointments"), axios.get("api/interviewers")]).then((all) => {
+      setState(prev => {
+        console.log('state :>> ', { ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data });
+        return ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data })
+      }
+      );
+    })
+  }, []);
+
+
+  // set the selected day from days
+  const setDay = day => setState(prev => ({ ...prev, day }));
+
+  const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
+
+
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+  }
+
+
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -36,8 +110,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
@@ -48,7 +122,16 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          // console.log("Line 131", appointment.interview)  
+          const interview = getInterview(state, appointment.interview);
+          return (<Appointment
+            key={appointment.id}
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview} interviewers={interviewers} bookInterview={bookInterview}/>)
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
